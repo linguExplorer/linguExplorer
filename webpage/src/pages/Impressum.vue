1a2b07669b8d1e9f7cce6a5839c1ff788e287d21
britta12345 <0060@htl.rennweg.at>
2024-11-01 17:49:42 +0100

    created impressum

---
+++ webpage/src/pages/Impressum.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+      <h1>Impressum</h1>
+      <p>Informationspflicht laut Offenlegungspflicht laut §25 Mediengesetz</p>
+      <h2>Websitebetreiber-Team:</h2>
+      <ul>
+        <li>Alexander Nems</li>
+        <li>Thuy Tien Luong</li>
+        <li>Benjamin Bician</li>
+        <li>Helena Stindl</li>
+        <li>Britta Reinwart</li>
+      </ul>
+      <h2>Inhalt:</h2>
+      <p>Diese Website dient zur Bereitstellung von Informationen über das Projekt "Linguexplorer", das im Rahmen einer Diplomarbeit an der HTL-Rennweg im Schuljahr 2024/25 entwickelt wurde. Linguexplorer ist eine Singleplayer-Webapplikation, die als Lernspiel konzipiert wurde und Schülerinnen und Schülern hilft, auf spielerische Weise die englische Sprache zu erlernen.</p>
+      <h2>Adresse:</h2>
+      <p>Rennweg 89b, 1030 Wien</p>
+      <h2>Kontaktdaten:</h2>
+      <p>team@linguexplorer.com</p>
+      <h2>Urheberrecht:</h2>
+      <p>Die Inhalte dieser Website sind urheberrechtlich geschützt. Jegliche Verwertung, die nicht vom Urheberrecht gedeckt ist, bedarf der schriftlichen Zustimmung des Betreibers.</p>
+      <h2>Haftung für Links:</h2>
+      <p>Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich. Sollten Sie rechtswidrige Inhalte auf verlinkten Seiten bemerken, informieren Sie uns bitte, damit wir die Links umgehend entfernen können.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Impressum',
+  }
+  </script>
+  
+  <style scoped>
+  .container {
+    padding: 20px;
+  }
+  h1 {
+    color: #f6f5f1;
+  }
+  </style>
+  
