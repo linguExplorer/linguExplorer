5f4866d42aa124c1ae2a4f18f2b433d48c930aeb
britta12345 <0060@htl.rennweg.at>
2024-11-01 17:51:29 +0100

    created datenschutz

---
+++ webpage/src/pages/Datenschutz.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="container">
+      <h1>Datenschutzerklärung</h1>
+      <h2>Allgemeine Hinweise:</h2>
+      <p>Der Schutz Ihrer persönlichen Daten ist uns sehr wichtig. Diese Datenschutzerklärung informiert Sie darüber, welche Daten wir erheben, wie wir diese verarbeiten und zu welchem Zweck sie genutzt werden. Wir behandeln Ihre personenbezogenen Daten vertraulich und entsprechend der gesetzlichen Datenschutzvorschriften sowie dieser Datenschutzerklärung.</p>
+      <h2>Verantwortlich:</h2>
+      <ul>
+        <li>Alexander Nems</li>
+        <li>Thuy Tien Luong</li>
+        <li>Benjamin Bician</li>
+        <li>Helena Stindl</li>
+        <li>Britta Reinwart</li>
+      </ul>
+      <p>team@linguexplorer.com</p>
+      <p>Rennweg 89b, 1030 Wien</p>
+      <h2>Erhebung und Verarbeitung personenbezogener Daten:</h2>
+      <p>Wir erheben, verarbeiten und nutzen personenbezogene Daten (z. B. Name, E-Mail-Adresse), soweit dies für die Bereitstellung unserer Webapplikation und der damit verbundenen Services notwendig ist. Dies erfolgt in Übereinstimmung mit den geltenden Datenschutzvorschriften.</p>
+      <h2>Folgende Daten können erhoben werden:</h2>
+      <ul>
+        <li>Name und E-Mail-Adresse</li>
+        <li>Nutzungsdaten</li>
+        <li>Technische Daten</li>
+      </ul>
+      <h2>Zweck der Datenverarbeitung:</h2>
+      <p>Die erhobenen Daten dienen zur Bereitstellung unseres Lernspiels "Linguexplorer".</p>
+      <h2>Speicherung der Daten:</h2>
+      <p>Ihre Daten werden nur so lange gespeichert, wie es für die Erfüllung des jeweiligen Zwecks erforderlich ist.</p>
+      <h2>Weitergabe der Daten:</h2>
+      <p>Personenbezogene Daten werden nicht ohne Ihre ausdrückliche Zustimmung an Dritte weitergegeben, es sei denn, dies ist gesetzlich vorgeschrieben oder zur Vertragserfüllung notwendig.</p>
+      <h2>Rechte der betroffenen Personen:</h2>
+      <ul>
+        <li>Auskunft über Ihre bei uns gespeicherten Daten zu erhalten.</li>
+        <li>Berichtigung unrichtiger Daten zu verlangen.</li>
+        <li>Löschung Ihrer Daten zu fordern.</li>
+        <li>Der Verarbeitung Ihrer Daten zu widersprechen.</li>
+        <li>Eine Beschwerde bei der zuständigen Datenschutzbehörde einzureichen.</li>
+      </ul>
+      <h2>Änderungen der Datenschutzerklärung:</h2>
+      <p>Diese Datenschutzerklärung wird regelmäßig überprüft und aktualisiert, um den rechtlichen Anforderungen zu entsprechen oder um Änderungen in unseren Services abzubilden.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Datenschutz',
+  }
+  </script>
+  
+  <style scoped>
+  .container {
+    padding: 20px;
+  }
+  h1 {
+    color: #f6f5f1;
+  }
+  </style>
+  
