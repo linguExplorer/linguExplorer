fc0ba3d905497d9e46fa6a0e30a4340a71393c33
britta12345 <0060@htl.rennweg.at>
2024-11-03 15:09:06 +0100

    update style

--- webpage/src/pages/NewPassword.vue
+++ webpage/src/pages/NewPassword.vue
@@ -28,6 +28,7 @@
 </template>
 
 <script>
+import '../styles/NewPassword.css';
 export default {
     name: 'NewPassword',
     data() {
@@ -48,93 +49,3 @@ export default {
 };
 </script>
 
-<style scoped>
-@font-face {
-    font-family: 'VCR OSD Mono';
-    src: url('@/assets/fonts/vcr_osd_mono/VCR_OSD_MONO_1.001.ttf') format('truetype');
-    font-weight: normal;
-    font-style: normal;
-}
-
-.page-wrapper {
-    background-color: #f6f5f1;
-    min-height: 100vh;
-    width: 100vw;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: black;
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
-
-.content-box {
-    width: 40%; /*allgem box schmäler*/
-    padding: 20px;
-    text-align: center;
-    background-color: transparent;
-}
-
-.welcome-text {
-    text-align: center;         
-    font-family: 'Pixelsplitter', Arial, sans-serif;
-}
-
-.h1 {
-    text-align: center; 
-    font-size: 32px;
-    color: black;
-}
-
-.input-group {
-    margin-bottom: 15px;
-    width: 100%;
-    text-align: center;
-}
-
-.input-group label {
-    font-family: 'VCR OSD Mono', monospace;
-    font-size: 16px;
-    display: block;
-}
-
-input {
-    display: block;
-    margin: 10px auto;
-    padding: 10px;
-    width: 100%; /*eigabefelder schmäler*/
-    background-color: white;
-    border: 2px solid #9cb405;
-    color: black;
-    font-family: 'VCR OSD Mono', monospace;
-    box-sizing: border-box;
-}
-
-
-#passwordLabel {
-    text-align: left;
-}
-
-#confirmPasswordLabel {
-    text-align: left;
-}
-
-.submit-button {
-    background: none;
-    border: none;
-    padding: 0;
-    cursor: pointer;
-    margin-top: 25px;
-}
-
-.submit-button img {
-    width: 150px;
-    height: auto;
-}
-
-input:-webkit-autofill {
-    -webkit-box-shadow: 0 0 0px 1000px white inset;
-    box-shadow: 0 0 0px 1000px white inset;
-}
-</style>
