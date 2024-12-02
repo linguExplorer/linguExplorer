2d681b1a1fb28699c4eba143ac51b5966a0b03fe
britta12345 <0060@htl.rennweg.at>
2024-12-02 17:36:20 +0100

    Update AccInfoSeite.vue

--- webpage/src/pages/AccInfoSeite.vue
+++ webpage/src/pages/AccInfoSeite.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="font-vcr m-0 text-black w-full bg-[#f6f5f1] min-h-screen flex flex-col justify-between">
     <!-- Header -->
-    <header class="flex justify-start mt-20 px-40">
-      <h1 class="font-pixelsplitter text-[40px] mb-6">Accountverwaltung</h1>
+    <header class="flex justify-center items-center mt-10 px-4 sm:mt-20 sm:px-40">
+      <h1 class="font-pixelsplitter text-[20px] sm:text-[40px] mb-6 text-center">Accountverwaltung</h1>
     </header>
 
     <!-- Rechts oben Button -->
@@ -12,7 +12,7 @@
         <img
           src="@/assets/xx_Images/xx_Images/Buttons/red_X.png"
           alt="Nach oben"
-          class="max-h-[55px] hover:opacity-80"
+          class="max-h-[40px] sm:max-h-[55px] hover:opacity-80"
         />
         </router-link>
 
@@ -22,40 +22,39 @@
     <!-- Hauptbereich -->
     <main class="flex flex-col justify-start flex-grow px-20"> 
       <!-- Benutzername ändern -->
-      <div class="flex flex-col w-[700px] max-w-5xl bg-[#d5d6d8] p-8 shadow-lg mt-6 mr-60"> 
-        <label for="username" class="font-pixelsplitter text-[26px] mb-6">BENUTZERNAME ÄNDERN</label>
+       <!--w: breite der box-->
+      <div class="flex flex-col w-full max-w-[700px] bg-[#d5d6d8] p-4 sm:p-8 shadow-lg mt-6 sm:ml-60 mx-auto">
+        <label for="username" class="font-pixelsplitter text-[15px] sm:text-[26px] mb-4 sm:mb-6">BENUTZERNAME ÄNDERN</label>
         <input
           type="text"
           id="username"
           v-model="username"
-          class="p-2 bg-white border-[#9cb405] border-2 w-[450px] mb-4"
+          class="p-2 bg-white border-[#9cb405] border-2 w-full mb-4"
           placeholder="maxmust123"
         />
-        <p v-if="UserError" class="text-red-500 mt-2">
-                {{ UserError }}
-              </p>
         <div class="flex justify-end">
           <button @click="changeUser" class="hover-button">
             <img
               src="@/assets/xx_Images/xx_Images/Buttons/aenderungenSpeichern.png"
               alt="Benutzername speichern"
-              class="h-[50px] w-auto hover:opacity-80"
+              class="h-[40px] sm:h-[50px] w-auto hover:opacity-80"
             />
           </button>
         </div>
       </div>
 
+
       <!-- Passwort ändern -->
-      <div class="flex flex-col w-[700px] max-w-5xl bg-[#d5d6d8] p-8 shadow-lg mt-6 mr-60">
-        <h2 class="font-pixelsplitter text-[26px] mb-4">PASSWORT ÄNDERN</h2>
+      <div class="flex flex-col w-full max-w-[700px] bg-[#d5d6d8] p-4 sm:p-8 shadow-lg mt-6 sm:ml-60 mx-auto">
+        <h2 class="font-pixelsplitter text-[15px] sm:text-[26px] mb-4 sm:mb-6">PASSWORT ÄNDERN</h2>
 
         <div class="input-group flex flex-col mb-4">
-          <label for="password" class="text-[16px]">altes Passwort</label>
+          <label for="password" class="text-[16px]">Altes Passwort</label>
           <input
             type="password"
             id="password"
             v-model="password"
-            class="font-vcr bg-white border-[#9cb405] border-2 min-w-[500px] p-2"
+            class="font-vcr bg-white border-[#9cb405] border-2 w-full p-2"
             required
           />
           <p v-if="passwordError" class="text-red-500 mt-2">
@@ -64,23 +63,27 @@
         </div>
 
         <div class="input-group flex flex-col mb-4">
-          <label for="newPassword" class="text-[16px]">neues Passwort (mind. 8 Zeichen)</label>
+          <label for="newPassword" class="text-[16px]"
+            >Neues Passwort (mind. 8 Zeichen)</label
+          >
           <input
             type="password"
             id="newPassword"
             v-model="newPassword"
-            class="font-vcr bg-white border-[#9cb405] border-2 min-w-[500px] p-2"
+            class="font-vcr bg-white border-[#9cb405] border-2 w-full p-2"
             required
           />
         </div>
 
         <div class="input-group flex flex-col mb-4">
-          <label for="confirmPassword" class="text-[16px]">Passwort wiederholen</label>
+          <label for="confirmPassword" class="text-[16px]"
+            >Passwort wiederholen</label
+          >
           <input
             type="password"
             id="confirmPassword"
             v-model="confirmPassword"
-            class="font-vcr bg-white border-[#9cb405] border-2 min-w-[500px] p-2"
+            class="font-vcr bg-white border-[#9cb405] border-2 w-full p-2"
             required
           />
         </div>
@@ -96,13 +99,16 @@
       </div>
 
       <!-- E-Mail ändern -->
-      <div class="flex flex-col w-[700px] max-w-5xl bg-[#d5d6d8] p-8 shadow-lg mt-6 mr-60"> 
-        <label for="email" class="font-pixelsplitter text-[26px] mb-6">E-MAIL ÄNDERN</label>
+      <div class="flex flex-col w-full max-w-[700px] bg-[#d5d6d8] p-4 sm:p-8 shadow-lg mt-6 sm:ml-60 mx-auto">
+        <label
+          for="email"
+          class="font-pixelsplitter text-[15px] sm:text-[26px] mb-4 sm:mb-6">E-MAIL ÄNDERN
+        </label>
         <input
           type="email"
           id="email"
           v-model="email"
-          class="p-2 bg-white border-[#9cb405] border-2 w-[450px] mb-4"
+          class="p-2 bg-white border-[#9cb405] border-2 w-full mb-4"
           placeholder="max.mustermann@gmail.com"
         />
         <div class="flex justify-end">
@@ -116,10 +122,11 @@
         </div>
       </div>
     </main>
-    <footer class="mt-auto w-full flex justify-between items-center px-12 py-12 bg-[#f6f5f1]">
+
+    <footer class="mt-auto w-full flex justify-between items-center px-4 sm:px-12 py-6 sm:py-12 bg-[#f6f5f1]">
     <!-- Linker Button -->
     <div class="flex justify-start">
-      <button class="max-w-[150px]" @click="goBack">
+      <button class="max-w-[100px] sm:max-w-[150px]" @click="goBack">
         <router-link to="/" href="#">
           <img src="@/assets/xx_Images/xx_Images/Buttons/abmelden.png" alt="Zurück" />
         </router-link>
@@ -128,7 +135,7 @@
 
     <!-- Rechter Button -->
     <div class="flex justify-end">
-      <button class="max-w-[150px]" @click="goBack">
+      <button class="max-w-[100px] sm:max-w-[150px]" @click="goBack">
         <router-link to="/" href="#">
           <img src="@/assets/xx_Images/xx_Images/Buttons/Loeschen.png" alt="Zurück" />
         </router-link>
