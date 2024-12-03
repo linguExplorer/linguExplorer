7f133e646c6ed38000f9acd707b6a9fa7bb26271
britta12345 <0060@htl.rennweg.at>
2024-12-03 13:40:12 +0100

    update responsiveness

--- webpage/src/pages/Afterlogin.vue
+++ webpage/src/pages/Afterlogin.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="font-vcr m-0 text-black w-full bg-[#f6f5f1] min-h-screen flex flex-col justify-between">
     <!-- Header -->
-    <header class="flex justify-start mt-40 px-40">
+    <header class="flex justify-start mt-40 px-60 md:px-40">
       <!-- Logo links oben -->
       <img
         src="@/assets/xx_Images/xx_Images/wordmark/wordmark_scaled.png"
         alt="Linguexplorer"
-        class="w-8/12 max-w-[600px]"
+        class="w-8/12 max-w-[600px] sm:w-6/12 md:w-8/12"
       />
     </header>
 
@@ -18,7 +18,7 @@
         <img
           src="@/assets/xx_Images/xx_Images/Buttons/SettingsIcon.png"
           alt="Nach oben"
-          class="max-h-[100px] hover:opacity-80"
+          class="max-h-[40px] w-[40px] sm:max-h-[60px] sm:w-[60px] hover:opacity-80"
         />
       </router-link>
 
@@ -33,7 +33,7 @@
             <img
               src="@/assets/xx_Images/xx_Images/Buttons/neuesSpiel_green.png"
               alt="Jetzt spielen"
-              class="max-h-[55px] hover:opacity-80"
+              class="max-h-[45px] hover:opacity-80 sm:max-h-[55px]"
             />
           </button>
 
@@ -41,7 +41,7 @@
             <img
              src="@/assets/xx_Images/xx_Images/Buttons/spielstandLaden_green.png"
               alt="Spielstand laden"
-              class="max-h-[55px] hover:opacity-80"
+              class="max-h-[45px] hover:opacity-80 sm:max-h-[55px]"
             />
           </button>
         </div>
@@ -50,7 +50,7 @@
           <img
             src="@/assets/xx_Images/xx_Images/MainCharacterFrontAnimationv2.gif"
             alt="Character Animation"
-            class="w-[200px] md:w-[200px] lg:w-[250px]"
+            class="w-[150px] sm:w-[200px] md:w-[200px] lg:w-[250px]"
           />
         </div>
       </div>
