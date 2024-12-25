4138ab0c8aae2fe8c895a5ed67828166c243766a
aswa <alexander.nems@gmail.com>
2024-12-25 17:46:24 +0100

    movement test

--- core/src/main/kotlin/com/github/linguExplorer/screen/GameScreen.kt
+++ core/src/main/kotlin/com/github/linguExplorer/screen/GameScreen.kt
@@ -2,27 +2,84 @@ package com.github.linguExplorer.screen
 
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.Color
-import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
 
 class GameScreen : Screen {
 
-    private val font = BitmapFont()
-    private val batch = SpriteBatch()  // SpriteBatch erstellen
+    private val batch = SpriteBatch()
 
-    override fun show() {
-        // Initialisierung des Spiels (falls notwendig)
-    }
+    private val strawberry = Texture(Gdx.files.internal("assets/Minigames/Essen/phraseImages/test_strawberry.png"))
+    private val basket = Texture(Gdx.files.internal("assets/Minigames/Essen/phraseImages/pixel_basket.jpg"))
+
+    private var strawberryX = 200f
+    private var strawberryY = 200f
+
+    private val basketX = 300f
+    private val basketY = 300f
+
+    private var isDragging = false
+    private var offsetX = 0f
+    private var offsetY = 0f
+    private var isCollected = false
+    private var animationAlpha = 1f
+
+    override fun show() {}
 
     override fun render(delta: Float) {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT) // Bildschirm löschen
+        handleInput()
 
-        batch.begin()  // Beginnt das Zeichnen
-        font.color = Color.WHITE
-        font.draw(batch, "Hallo, LibGDX!", 100f, 200f)  // Text zeichnen
-        batch.end()  // Beendet das Zeichnen
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+        batch.begin()
+        batch.draw(basket, basketX, basketY)
+
+        if (!isCollected) {
+            batch.draw(strawberry, strawberryX, strawberryY)
+        } else {
+            if (animationAlpha > 0) {
+                batch.setColor(1f, 1f, 1f, animationAlpha)
+                batch.draw(strawberry, strawberryX, strawberryY)
+                batch.setColor(1f, 1f, 1f, 1f)
+                animationAlpha -= delta
+            }
+        }
+
+
+
+        batch.end()
+    }
+
+    private fun handleInput() {
+        val mouseX = Gdx.input.x.toFloat()
+        val mouseY = Gdx.graphics.height - Gdx.input.y.toFloat()
+
+        if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
+            if (!isDragging && isMouseInsideImage(mouseX, mouseY, strawberryX, strawberryY, strawberry)) {
+                isDragging = true
+                offsetX = mouseX - strawberryX
+                offsetY = mouseY - strawberryY
+            }
+
+            if (isDragging && !isCollected) {
+                strawberryX = mouseX - offsetX
+                strawberryY = mouseY - offsetY
+            }
+        } else {
+            if (isDragging) {
+                // Überprüfen, ob Erdbeere im Korb ist
+                if (isMouseInsideImage(strawberryX, strawberryY, basketX, basketY, basket)) {
+                    isCollected = true // Sammelstatus setzen
+                }
+            }
+            isDragging = false
+        }
+    }
+
+    private fun isMouseInsideImage(mouseX: Float, mouseY: Float, imageX: Float, imageY: Float, image: Texture): Boolean {
+        return mouseX in imageX..(imageX + image.width) && mouseY in imageY..(imageY + image.height)
     }
 
     override fun resize(width: Int, height: Int) {}
@@ -34,7 +91,8 @@ class GameScreen : Screen {
     override fun resume() {}
 
     override fun dispose() {
-        batch.dispose()  // SpriteBatch und andere Ressourcen freigeben
-        font.dispose()
+        batch.dispose()
+        strawberry.dispose()
+        basket.dispose()
     }
 }
