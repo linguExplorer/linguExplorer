1e8b2d207eebaea4bd143246930f14fcefe7890c
britta12345 <0060@htl.rennweg.at>
2025-02-16 14:26:55 +0100

    create game menü

--- core/src/main/kotlin/com/github/linguExplorer/linguExplorer.kt
+++ core/src/main/kotlin/com/github/linguExplorer/linguExplorer.kt
@@ -2,6 +2,7 @@ package com.github.linguExplorer
 
 import com.badlogic.gdx.Application
 import com.badlogic.gdx.Gdx
+import com.github.linguExplorer.screen.GameMenuScreen
 import com.github.linguExplorer.screen.MainMenuScreen
 import com.github.linguExplorer.screen.MapScreen
 import ktx.app.KtxGame
@@ -10,11 +11,15 @@ import ktx.app.KtxScreen
 class linguExplorer : KtxGame<KtxScreen>() {
 
     override fun create() {
-        Gdx.app.logLevel = Application.LOG_DEBUG // Debug mode aktivieren
-        addScreen(MainMenuScreen(this)) // Setzt den GameScreen
-        setScreen<MainMenuScreen>()
+        Gdx.app.logLevel = Application.LOG_DEBUG // Debug-Modus aktivieren
+        // addScreen(MainMenuScreen(this)) // Setzt den GameScreen
+        // setScreen<MainMenuScreen>()
+
+        addScreen(GameMenuScreen(this)) // Füge den GameMenuScreen hinzu
+        setScreen<GameMenuScreen>() // Setze den GameMenuScreen als aktuellen Screen
     }
+
     companion object {
-        const val UNIT_SCALE = 1/16f
+        const val UNIT_SCALE = 1 / 16f
     }
 }
