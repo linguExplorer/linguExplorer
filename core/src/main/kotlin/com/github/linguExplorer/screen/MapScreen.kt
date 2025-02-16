16c5168ae04f5f44e93751731a6ce33d8307328a
Dingo337 <bbicianpv@gmail.com>
2025-02-16 20:43:28 +0100

    New Character

--- core/src/main/kotlin/com/github/linguExplorer/screen/MapScreen.kt
+++ core/src/main/kotlin/com/github/linguExplorer/screen/MapScreen.kt
@@ -1,5 +1,7 @@
 package com.github.linguExplorer.screen
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.InputMultiplexer
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.TextureAtlas
 import com.badlogic.gdx.maps.tiled.TiledMap
@@ -80,9 +82,68 @@ class MapScreen(private val game: linguExplorer, private val tempX: Float, priva
         stage.fire(MapChangeEvent(currentMap!!))
 
 
+        // fixe Bilder hinzufügen
+        addUIImages()
+
         PlayerKeyboardInputProcessor(world, stage, world.mapper(), world.mapper(), stage, pathSystem )
 
+        val playerInputProcessor = PlayerKeyboardInputProcessor(world, stage, world.mapper(), world.mapper(), stage, pathSystem)
+        // InputMultiplexer um Spielfigur + UI zu verarbeiten
+        val inputMultiplexer = InputMultiplexer()
+        inputMultiplexer.addProcessor(playerInputProcessor)
+        inputMultiplexer.addProcessor(stage)   // Spielfigur und Welt-Stage
+        //inputMultiplexer.addProcessor(uiStage) // UI-Stage*/
+
+        Gdx.input.inputProcessor = inputMultiplexer //Multiplexer als Input-Prozessor setzen
     }
+
+    private val uiStage: Stage = Stage(ExtendViewport(16f, 9f)) // Eine Stage speziell für die UI
+
+    //fixe Bilder Methode
+    private fun addUIImages() {
+        // Bilder laden
+        val backpackTexture = Texture("graphics/map-objects/Rucksack/v2/Backpack2.png")
+        val mapTexture = Texture("graphics/map-objects/Map/v2/Map2.png")
+        val phrasingBookTexture = Texture("graphics/map-objects/Phrasenheft/v2/Phrasenheft2-1.png.png")
+        //val progressBarTexture = Texture("graphics/map-objects/Prozentleiste/v2/Prozentleiste2-1.png.png")
+        val moneyBagTexture = Texture("graphics/map-objects/Coinbag/v2/MoneyBag2-2.png.png")
+
+        // Images für jedes Bild
+        val backpackImage = com.badlogic.gdx.scenes.scene2d.ui.Image(backpackTexture)
+        val mapImage = com.badlogic.gdx.scenes.scene2d.ui.Image(mapTexture)
+        val phrasingBookImage = com.badlogic.gdx.scenes.scene2d.ui.Image(phrasingBookTexture)
+        //val progressBarImage = com.badlogic.gdx.scenes.scene2d.ui.Image(progressBarTexture)
+        val moneyBagImage = com.badlogic.gdx.scenes.scene2d.ui.Image(moneyBagTexture)
+
+        // Größe
+        val imageSize = 2f //Definiere die größe für die Bilder
+        backpackImage.setSize(imageSize, imageSize)
+        mapImage.setSize(imageSize, imageSize)
+        phrasingBookImage.setSize(imageSize, imageSize)
+        //progressBarImage.setSize(imageSize, imageSize-1f)
+        moneyBagImage.setSize(imageSize, imageSize)
+
+        // Position
+        backpackImage.setPosition(12.8f, 7.2f)  // rechts oben
+        mapImage.setPosition(0f, 6.5f)     // links oben
+        phrasingBookImage.setPosition(14.5f, 0.5f) // rechts unten
+        //progressBarImage.setPosition(0f, 8f) // Beispiel
+        moneyBagImage.setPosition(14.5f, 7f)   // rechts oben
+
+        // Bilder zur UI-Stage hinzufügen
+        uiStage.addActor(backpackImage)
+        uiStage.addActor(mapImage)
+        uiStage.addActor(phrasingBookImage)
+        //uiStage.addActor(progressBarImage)
+        uiStage.addActor(moneyBagImage)
+    }
+
+
+
+
+
+
+
 
     override fun resize(width: Int, height: Int) {
         stage.viewport.update(width, height, true)
@@ -91,6 +152,16 @@ class MapScreen(private val game: linguExplorer, private val tempX: Float, priva
     override fun render(delta: Float) {
 
         world.update(delta.coerceAtMost(0.25f))
+
+
+        // Zeichne die Welt (z.B. Karte)
+        stage.act(Math.min(delta, 1 / 30f)) // Update für die Haupt-Stage
+        stage.draw()
+
+        // Zeichne die UI-Stage
+        uiStage.act(Math.min(delta, 1 / 30f)) // Update für die UI-Stage
+        uiStage.draw() // UI immer über der Welt
+
     }
 
     override fun dispose() {
--- core/src/main/kotlin/com/github/linguExplorer/system/EntitySpawnSystem.kt
+++ core/src/main/kotlin/com/github/linguExplorer/system/EntitySpawnSystem.kt
@@ -104,8 +104,16 @@ class EntitySpawnSystem (
         if(regions.isEmpty) {
             gdxError("No Regions for $model")
         }
+
+
         val firstFrame = regions.first()
-        vec2(firstFrame.originalWidth* (UNIT_SCALE/16), firstFrame.originalHeight* (UNIT_SCALE/16))
+        if(model.atlasKey == "nc") {
+            vec2(firstFrame.originalWidth* (UNIT_SCALE), firstFrame.originalHeight* (UNIT_SCALE))
+
+        } else {
+            vec2(firstFrame.originalWidth* (UNIT_SCALE/16), firstFrame.originalHeight* (UNIT_SCALE/16))
+
+        }
     }
 
     private fun spawnCfg(type:String):SpawnCfg = cachedCfgs.getOrPut(type) {
@@ -116,20 +124,21 @@ class EntitySpawnSystem (
                 aniType = AnimationType.RIGHT
 
             )
+
+            "NC_1" -> SpawnCfg(AnimationModel.NC,
+                physicScaling = vec2(0.8f,0.5f),
+                physicOffset = vec2(0f,-6f* UNIT_SCALE),
+                aniType = AnimationType.IDLE
+
+            )
             else -> gdxError("Type $type no Spawn config")
         }
+
+
     }
 
     override fun handle(event: Event): Boolean {
         when (event) {
-
-                is GameEndEvent -> {
-
-                    println("[GameEndEvent] Received. Setting tempLocation.")
-                    //tempLocation = 31.104187f to 15.677063f
-                   // println(tempLocation)
-                    return true
-                }
 
 
             is MapChangeEvent -> {
@@ -140,16 +149,32 @@ class EntitySpawnSystem (
                     mapObj ->
                     val type = mapObj.type ?: gdxError("MapObject $mapObj no type")
                     world.entity {
-                        add<SpawnComponent> {
-                            this.type = type
-                            this.location.set(
 
-                                tempX ?: (mapObj.x * UNIT_SCALE),
-                                tempY ?: (mapObj.y * UNIT_SCALE)
+                        if (type == "Player") {
+                            add<SpawnComponent> {
+                                this.type = type
+                                this.location.set(
 
-                            )
-                            println(this.location)
+                                    tempX ,
+                                    tempY
+
+                                )
+                                println("Playe found and spawned")
+                            }
+                        } else {
+                            add<SpawnComponent> {
+                                this.type = type
+                                this.location.set(
+
+                                   (mapObj.x * UNIT_SCALE),
+                                    (mapObj.y * UNIT_SCALE)
+
+                                )
+                            }
                         }
+
+
+
                     }
                 }
 
