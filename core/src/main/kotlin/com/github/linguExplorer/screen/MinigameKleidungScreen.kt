515bc6714d91f9e5c852d680a542fb4d47cd46ef
Dingo337 <bbicianpv@gmail.com>
2025-03-05 08:52:14 +0100

    Games in Map

--- core/src/main/kotlin/com/github/linguExplorer/screen/MinigameKleidungScreen.kt
+++ core/src/main/kotlin/com/github/linguExplorer/screen/MinigameKleidungScreen.kt
@@ -638,7 +638,7 @@ class MinigameKleidungScreen(private val game: linguExplorer) : KtxScreen {
                     if (game!!.containsScreen<MapScreen>()) {
                         game.removeScreen<MapScreen>()
                     }
-                    game.addScreen(MapScreen(game, 10f, 10f))
+                    game.addScreen(MapScreen(game,  26.5f, 4.6f))
                     game.setScreen<MapScreen>()
                 }
             }
