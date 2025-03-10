d8b1fe341bfd4b376eb4a959b4a79bcb8eb6eb53
aswa <alexander.nems@gmail.com>
2025-03-10 21:32:10 +0100

    Update DatabaseGameMethods.kt

--- core/src/main/kotlin/com/github/linguExplorer/database/DatabaseGameMethods.kt
+++ core/src/main/kotlin/com/github/linguExplorer/database/DatabaseGameMethods.kt
@@ -2,19 +2,20 @@ package com.github.linguExplorer.database
 
 import com.github.linguExplorer.models.*
 import com.github.linguExplorer.repositories.*
+import com.github.linguExplorer.saveNumber
 import org.jetbrains.exposed.sql.transactions.transaction
 
-/*fun checkIfTopicAvailable(userId: Int, topicId: Int): Boolean {
+fun checkIfTopicAvailable(userId: Int, topicId: Int): Boolean {
     return transaction {
         TopicRepository().getTopicById(topicId) ?: return@transaction false
 
         if (topicId == TopicRepository().getFirstTopic().id) return@transaction true
 
-        val currentProgress = UserProgressRepository().getUserProgress(userId, topicId)
+        val currentProgress = UserProgressRepository().getUserProgress(userId, saveNumber, topicId)
         if (currentProgress != null) return@transaction true
 
         val previousTopic = TopicRepository().getTopicById(topicId - 1) ?: return@transaction false
 
         return@transaction false
     }
-}*/
+}
