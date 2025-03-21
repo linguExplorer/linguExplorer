7b46936d0aa2278366dfec43dc1eec4af6f38fda
britta12345 <0060@htl.rennweg.at>
2025-03-21 17:17:32 +0100

    update impressum

--- webpage/src/pages/Impressum.vue
+++ webpage/src/pages/Impressum.vue
@@ -4,11 +4,11 @@
     <!-- Header -->
     <header class="bg-[#99b305] text-black sticky top-0 z-10">
       <section class="w-full py-2 flex justify-between items-center px-4">
-          <img
-            src="@/assets/xx_Images/xx_Images/wordmark/wordmark_hell_scaled.png"
-            alt="Logo"
-            class="w-3/6 max-w-[180px] sm:max-w-[200px] mr-9"
-          />
+        <img
+          src="@/assets/xx_Images/xx_Images/wordmark/wordmark_hell_scaled.png"
+          alt="Logo"
+          class="w-3/6 max-w-[180px] sm:max-w-[200px] mr-9"
+        />
         <div class="flex justify-center items-center space-x-2 sm:space-x-4">
           <!-- Button -->
           <button class="nav-button-spielbeschreibung hover-button">
@@ -66,6 +66,151 @@
         <p>
           Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich. Sollten Sie rechtswidrige Inhalte auf verlinkten Seiten bemerken, informieren Sie uns bitte, damit wir die Links umgehend entfernen können.
         </p>
+
+        <!-- Soundeffekt Quellen -->
+        <h2 class="font-bold mt-6">Soundeffekt-Quellen:</h2>
+        <ul class="list-disc pl-5">
+          <li>
+            <b>Achievement:</b> arcade-ui-18-229517.mp3
+            <br>
+            Sound Effect by <a href="https://pixabay.com/de/users/floraphonic-38928062/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=229517" target="_blank">floraphonic</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=229517" target="_blank">Pixabay</a>
+          </li>
+          <li>
+            <b>Auswählen:</b> arcade-ui-1-229498
+            <br>
+            Sound Effect by <a href="https://pixabay.com/de/users/floraphonic-38928062/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=229517" target="_blank">floraphonic</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=229517" target="_blank">Pixabay</a>
+          </li>
+          <li>
+            <b>:</b>arcade-ui-6-229503
+            <br>
+            Sound Effect by <a href="https://pixabay.com/de/users/floraphonic-38928062/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=229517" target="_blank">floraphonic</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=229517" target="_blank">Pixabay</a>
+          </li>
+          <li>
+            <b>Benachrichtigung:</b> 
+            <br>
+            Sound Effect by <a href="https://pixabay.com/users/rasoolasaad-47313572/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=269288" target="_blank">Rasool Asaad</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=269288" target="_blank">Pixabay</a>
+          </li>
+          <li>
+            <b>Bumping:</b> Pixel-sound-effect-4-82881
+            <br>
+            Sound Effect by <a href="https://pixabay.com/users/freesound_community-46691455/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=82881" target="_blank">freesound_community</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=82881" target="_blank">Pixabay</a>
+          </li>
+          <li>
+            <b>:</b> pixel-sound-effect-5-103221
+            <br>
+            Sound Effect by <a href="https://pixabay.com/users/freesound_community-46691455/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=103221" target="_blank">freesound_community</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=103221" target="_blank">Pixabay</a>
+          </li>
+          <li>
+            <b>ButtonNichtVerfügbar:</b> 
+            <br>
+            Sound Effect by <a href="https://pixabay.com/users/rasoolasaad-47313572/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=269282" target="_blank">Rasool Asaad</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=269282" target="_blank">Pixabay</a>
+          </li>
+          <li>
+            <b>Dog:</b> 
+            <br>
+            Sound Effect by <a href="https://pixabay.com/de/users/u_5wgfa0ekjt-41084060/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=179915" target="_blank">u_5wgfa0ekjt</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=179915" target="_blank">Pixabay</a>
+          </li>
+          <li>
+            <b>Entwählen:</b> 
+            <br>
+            Sound Effect by <a href="https://pixabay.com/users/floraphonic-38928062/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=188105" target="_blank">floraphonic</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=188105" target="_blank">Pixabay</a>
+          </li>
+          <li>
+            <b>FalscheAntwort:</b> arcade-ui-4-229502
+            <br>
+            Sound Effect by <a href="https://pixabay.com/nl/users/floraphonic-38928062/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=229502" target="_blank">floraphonic</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=229502" target="_blank">Pixabay</a>
+          </li>
+          <li>
+            <b>:</b> arcade-ui-5-229504
+            <br>
+            Sound Effect by <a href="https://pixabay.com/fr/users/floraphonic-38928062/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=229504" target="_blank">floraphonic</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=229504" target="_blank">Pixabay</a>
+          </li>
+          <li>
+            <b>Katze:</b> cat-meow-8-fx-306184
+            <br>
+            Sound Effect by <a href="https://pixabay.com/ro/users/sound_garage-47313534/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=306184" target="_blank">SOUND_GARAGE</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=306184" target="_blank">Pixabay</a>
+          </li>
+          <li>
+            <b>PopUp:</b> tap-notification-180637
+            <br>
+            Sound Effect by <a href="https://pixabay.com/users/lucadialessandro-25927643/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=180637" target="_blank">Luca Di Alessandro</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=180637" target="_blank">Pixabay</a>
+          </li>
+          <li>
+            <b>Raketenabsturz:</b> 
+            <br>
+            Sound Effect by <a href="https://pixabay.com/de/users/freesound_community-46691455/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=89534" target="_blank">freesound_community</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=89534" target="_blank">Pixabay</a>
+          </li>
+          <li>
+            <b>richtigeAntwort:</b> 90s-game-ui-6-185099
+            <br>
+            Sound Effect by <a href="https://pixabay.com/users/floraphonic-38928062/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=185099" target="_blank">floraphonic</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=185099" target="_blank">Pixabay</a>
+          </li>
+          <li>
+            <b>SpeicherstandAuswählen:</b> 
+            <br>
+            Sound Effect by <a href="https://pixabay.com/users/notification_message-47259947/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=270130" target="_blank">Devrinta Rose Nataya</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=270130" target="_blank">Pixabay</a>
+          </li>
+          <li>
+            <b>startSound:</b> 
+            <br>
+            Sound Effect by <a href="https://pixabay.com/de/users/freesound_community-46691455/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=6104" target="_blank">freesound_community</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=6104" target="_blank">Pixabay</a>
+          </li>
+          <li>
+            <b>TestSound:</b> 
+            <br>
+            Sound Effect by <a href="https://pixabay.com/users/notification_message-47259947/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=270134" target="_blank">Devrinta Rose Nataya</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=270134" target="_blank">Pixabay</a>
+          </li>
+          <li>
+            <b>Umblättern:</b> page-flip-47177
+            <br>
+            Sound Effect by <a href="https://pixabay.com/users/freesound_community-46691455/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=47177" target="_blank">freesound_community</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=47177" target="_blank">Pixabay</a>
+          </li>
+          <li>
+            <b>X_Knopf:</b> 
+            <br>
+            Sound Effect by <a href="https://pixabay.com/users/rasoolasaad-47313572/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=269289" target="_blank">Rasool Asaad</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=269289" target="_blank">Pixabay</a>
+          </li>
+           <li>
+            <b>Sprechsound:</b> Alien-talking-312011
+            <br>
+            Sound Effect by <a href="https://pixabay.com/users/artificiallyinspired-30441549/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=312011" target="_blank">ArtificiallyInspired</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=312011" target="_blank">Pixabay</a>
+          </li>
+           <li>
+            <b>:</b> Alien-high-pitch-312010
+            <br>
+            Sound Effect by <a href="https://pixabay.com/users/artificiallyinspired-30441549/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=312010" target="_blank">ArtificiallyInspired</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=312010" target="_blank">Pixabay</a>
+          </li>
+           <li>
+            <b>:</b> Cute-alien
+            <br>
+            Sound Effect by <a href="https://pixabay.com/users/artificiallyinspired-30441549/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=291369" target="_blank">ArtificiallyInspired</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=291369" target="_blank">Pixabay</a>
+          </li>
+           <li>
+            <b>Minispiel_Essen:</b> 
+            <br>
+            Music by <a href="https://pixabay.com/ru/users/backgroundmusicforvideo-46826676/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=263126" target="_blank">BackgroundMusicForVideo</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=263126" target="_blank">Pixabay</a>
+          </li>
+           <li>
+            <b>Minispiel_Kleidung:</b> 
+            <br>
+            Music by <a href="https://pixabay.com/de/users/music_for_videos-26992513/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=156936" target="_blank">Anastasia Chubarova</a> from <a href="https://pixabay.com/music//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=156936" target="_blank">Pixabay</a>
+          </li>
+           <li>
+            <b>Minispiel_Schule:</b> 
+            <br>
+            Music by <a href="https://pixabay.com/de/users/crab_audio-47493304/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=300579" target="_blank">EVGENY</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=300579" target="_blank">Pixabay</a>
+          </li>
+           <li>
+            <b>Minispiel_Familie:</b> 
+            <br>
+            Music by <a href="https://pixabay.com/de/users/goldensoundlabs-31886162/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=222524" target="_blank">Golden Sound Labs</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=222524" target="_blank">Pixabay</a>
+          </li>
+           <li>
+            <b>Hintergrundmusik:</b> 
+            <br>
+            Music by <a href="https://pixabay.com/de/users/monument_music-34040748/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=194622" target="_blank">Oleksii Holubiev</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=194622" target="_blank">Pixabay</a>
+          </li>
+        </ul>
       </div>
     </main>
 
